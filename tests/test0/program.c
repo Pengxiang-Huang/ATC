5bfa6991e1886ce7e7d46891c9b40bdc070ad851
Pengxiang Huang <huangpengxiang70@gmail.com>
2024-05-22 17:51:07 -0500

    change to a simple sample

--- tests/test0/program.c
+++ tests/test0/program.c
@@ -1,9 +1,25 @@
 #include "HALIDE.h"
+#include <stdio.h>
 
 int main(){
 
-	HALIDE_DATA d = HALIDE_new(5, 8);
+	HALIDE_DATA d = HALIDE_new(5, 0);
 	
+	for (int i = 0 ; i < 5; ++i){
+		for (int j = 0; j < 5; ++j ){
+			HALIDE_update(d, i , j, 5);				
+		}
+	}
+	
+	for (int k = 0; k < 5; ++k){
+		for (int m = 0 ; m < 5; ++m){
+			HALIDE_update(d, k, m, 10);
+		}
+	}
+
+	printf("the data is: %ld \n", HALIDE_get(d, 1, 2));
+
+	HALIDE_free(d);
 
 	return 0;
 }
