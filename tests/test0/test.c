3031d4139061774a7ec8abd29c41bcc6f396543b
Pengxiang Huang <huangpengxiang70@gmail.com>
2024-05-25 21:08:07 -0500

    add one test case

--- tests/test0/test.c
+++ tests/test0/test.c
@@ -3,23 +3,25 @@
 
 int main(){
 
-	HALIDE_DATA d = HALIDE_new(5, 0);
-	
+	HALIDE_DATA d0 = HALIDE_new(5, 0);
+	HALIDE_DATA d1 = HALIDE_new(5, 0);
+
 	for (int i = 0 ; i < 5; ++i){
 		for (int j = 0; j < 5; ++j ){
-			HALIDE_update(d, i , j, 5);				
+			HALIDE_add(d0, d1, i, j, 3);				
 		}
 	}
 	
 	for (int k = 0; k < 5; ++k){
 		for (int m = 0 ; m < 5; ++m){
-			HALIDE_update(d, k, m, 10);
+			HALIDE_sub(d0, d1, k, m, 2);
 		}
 	}
 
-	printf("the data is: %ld \n", HALIDE_get(d, 1, 2));
+	printf("the data is: %ld \n", HALIDE_get(d1, 1, 2));
 
-	HALIDE_free(d);
+	HALIDE_free(d0);
+	HALIDE_free(d1);
 
 	return 0;
 }
